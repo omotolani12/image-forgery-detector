2e2b70be1185cf13d7c483081e4e14393c2da905
Omotolani Kehinde <68435127+omotolani12@users.noreply.github.com>
2025-05-02 23:18:20 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -81,4 +81,5 @@ async def predict_image(file: UploadFile = File(...)):
 
 # Local run
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+
